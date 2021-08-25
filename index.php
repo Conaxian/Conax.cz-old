14a16ee39aefb4d6eb0d0acce7d1566a4623f37e
Conaxian <accounts2@jeniweb.eu>
2021-08-25 15:54:12 +0200

    Add userdata

--- index.php
+++ index.php
@@ -12,16 +12,7 @@
   ?>
   <body>
 
-  <div class="container">
-    <nav class="topnav">
-      <a href="#">A</a>
-      <a href="#">B</a>
-      <a href="#">C</a>
-      <a href="#">D</a>
-    </nav>
-    <h1>Test Heading</h1>
-    <p>Test Paragraph</p>
-  </div>
+  <p>Coming soon</p>
 
   </body>
 </html>
