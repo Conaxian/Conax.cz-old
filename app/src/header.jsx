f7d9c72804afce808a6ea4581595f9f67a48f5a8
Conaxian <accounts2@jeniweb.eu>
2021-08-30 19:20:54 +0200

    Add header classes and title

--- app/src/header.jsx
+++ app/src/header.jsx
@@ -2,19 +2,39 @@ import React from "react";
 import {
   AppBar,
   Toolbar,
-  IconButton
+  IconButton,
+  Typography,
+  makeStyles
 } from "@material-ui/core";
 import { Menu as MenuIcon } from "@material-ui/icons";
+import theme from "./theme.jsx";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1,
+    display: "none"
+  }
+};
 
 class Header extends React.Component {
   render() {
     return (
       <>
-        <AppBar position="fixed">
+        <AppBar position="fixed" className={styles.root}>
           <Toolbar>
-            <IconButton edge="start" aria-label="menu">
+            <IconButton edge="start" classNmae={styles.menuButton}
+            aria-label="menu">
               <MenuIcon />
             </IconButton>
+            <Typography className={styles.title} variant="h1">
+              Conax
+            </Typography>
           </Toolbar>
         </AppBar>
         <Toolbar />
