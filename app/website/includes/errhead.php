67aaf2b915d8fbf4610fec400da692ac45330f4c
Conaxian <accounts2@jeniweb.eu>
2021-08-30 21:52:17 +0200

    Remove error head comments

--- app/website/includes/errhead.php
+++ app/website/includes/errhead.php
@@ -1,12 +1,10 @@
 <head>
 
-  <!-- General Meta -->
   <title><?php print $error;?></title>
   <meta charset="utf-8">
   <meta name="author" content="Conax">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Favicon -->
   <link rel="shortcut icon" type="image/png"
   href="/images/icons/favicon.png">
 
