681ddb624ce3150ab239a071e8c6ad01e2ad427b
Conaxian <accounts2@jeniweb.eu>
2021-08-30 23:05:08 +0200

    Change syntax in app

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./header.jsx";
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div className="app">
@@ -10,3 +10,5 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default App;
