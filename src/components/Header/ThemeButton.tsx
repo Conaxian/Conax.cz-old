843b858228683ae0bbd83927a6a910bcc4c527a0
Conaxian <conax@conax.cz>
2021-10-09 18:13:57 +0200

    Add ThemeButton component

---
+++ src/components/Header/ThemeButton.tsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { IconButton } from "@mui/material";
+import {
+  LightMode as LightModeIcon,
+  DarkMode as DarkModeIcon,
+} from "@mui/icons-material";
+
+import { ThemeContext } from "../Theme";
+
+class ThemeButton extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    return (
+    <ThemeContext.Consumer>
+      {({ theme, toggleTheme }) => (
+      <IconButton color="inherit" sx={{ marginLeft: 1 }}
+      onClick={ toggleTheme }>
+        { theme === "light" ? <LightModeIcon /> : <DarkModeIcon /> }
+      </IconButton>
+      )}
+    </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default ThemeButton;
