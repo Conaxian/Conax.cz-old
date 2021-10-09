b380d273062d17e33a15a8455ce2ce28e36db839
Conaxian <conax@conax.cz>
2021-10-09 15:24:11 +0200

    Add School Notes model

---
+++ web/models/SchoolNotes.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+const SCHOOL_REPO = "Conaxian/School";
+
+class SchoolNotesMod {
+  public function getNote(string $year, string $subject, string | int $number) {
+    $apiEndpoint = "/repos/" . SCHOOL_REPO .
+      "/contents/$year/$subject/notes/$number.md";
+    $githubApi = new GithubApiMod();
+    $note = $githubApi->request($apiEndpoint, "RAW");
+    return $note;
+  }
+}
+
+?>
