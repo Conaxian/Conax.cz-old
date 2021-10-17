7deff6b3613be78299207b477a74ac3f799a74c1
Conaxian <conax@conax.cz>
2021-10-17 19:56:33 +0200

    Increase NotePage bottom margin

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -14,7 +14,7 @@ class NotePage extends Component {
   render() {
     return (
       <>
-        <Box sx={{ mx: 4, flexGrow: 1, mb: 6 }}>
+        <Box sx={{ mx: 4, flexGrow: 1, mb: 8 }}>
           <Markdown text={this.props.note} />
         </Box>
         <NoteNav />
