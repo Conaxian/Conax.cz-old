315d6c5d33c6905322d2d14a58ca5afce17d9f09
Conaxian <conax@conax.cz>
2021-10-17 19:47:47 +0200

    Improve NoteNav style

--- src/components/NoteNav/index.tsx
+++ src/components/NoteNav/index.tsx
@@ -1,32 +1,29 @@
 import { Component } from "react";
-import { Box, BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { Paper, BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { Restore as RestoreIcon } from "@mui/icons-material";
 
 class NoteNav extends Component {
   render() {
     return (
-      <Box sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-        <BottomNavigation color="primary">
+      <Paper sx={{ position: "fixed", width: "100%", bottom: 0 }} elevation={3}>
+        <BottomNavigation showLabels>
           <BottomNavigationAction
             label="Previous"
             href="/notes"
-            color="primary"
             icon={<RestoreIcon />}
           />
           <BottomNavigationAction
             label="Subjects"
             href="/notes"
-            color="primary"
             icon={<RestoreIcon />}
           />
           <BottomNavigationAction
             label="Next"
             href="/notes"
-            color="primary"
             icon={<RestoreIcon />}
           />
         </BottomNavigation>
-      </Box>
+      </Paper>
     );
   }
 }
