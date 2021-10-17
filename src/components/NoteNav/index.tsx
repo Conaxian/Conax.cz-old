2f8a90483956c104f48e72cf83163c0d291e49e9
Conaxian <conax@conax.cz>
2021-10-17 19:06:22 +0200

    Make NoteNav have primary color

--- src/components/NoteNav/index.tsx
+++ src/components/NoteNav/index.tsx
@@ -6,7 +6,7 @@ class NoteNav extends Component {
   render() {
     return (
       <Box sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-        <BottomNavigation>
+        <BottomNavigation color="primary">
           <BottomNavigationAction
             label="Previous"
             href="/notes"
