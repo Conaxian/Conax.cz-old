4e34c61fc9216fcb9c137b2b0f77e9b40fe42a43
Conaxian <conax@conax.cz>
2021-10-17 22:23:46 +0200

    Make Link inherit its color in TitleText

--- src/components/Header/TitleText.tsx
+++ src/components/Header/TitleText.tsx
@@ -5,7 +5,7 @@ class TitleText extends Component {
   render() {
     return (
       <Typography variant="h6" noWrap sx={{ flexGrow: 1, fontWeight: 700 }}>
-        <Link href="/" underline="none" sx={{ color: "text" }}>
+        <Link href="/" underline="none" color="inherit">
           Conax
         </Link>
       </Typography>
