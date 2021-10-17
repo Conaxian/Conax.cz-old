86599bf40a5ce24ea0200e23dd5ee10cca9a5a1c
Conaxian <conax@conax.cz>
2021-10-17 22:18:04 +0200

    Change Link color in TitleText

--- src/components/Header/TitleText.tsx
+++ src/components/Header/TitleText.tsx
@@ -5,7 +5,7 @@ class TitleText extends Component {
   render() {
     return (
       <Typography variant="h6" noWrap sx={{ flexGrow: 1, fontWeight: 700 }}>
-        <Link href="/" underline="none">
+        <Link href="/" underline="none" sx={{ color: "primary" }}>
           Conax
         </Link>
       </Typography>
