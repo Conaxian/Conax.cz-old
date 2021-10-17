7b4151045c445229f9a84205a932b13d05d55dc8
Conaxian <conax@conax.cz>
2021-10-17 14:40:46 +0200

    Add NoteMenu page

---
+++ src/pages/NoteMenuPage.tsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import { Box } from "@mui/material";
+
+import SubjectsNav from "../components/SubjectsNav";
+
+interface Props {
+  note: string;
+}
+
+class NotePage extends Component {
+  props!: Readonly<Props>;
+
+  render() {
+    return (
+      <Box sx={{ marginX: 4 }}>
+        <SubjectsNav />
+      </Box>
+    );
+  }
+}
+
+export default NotePage;
