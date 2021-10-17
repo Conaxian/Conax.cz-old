bba5ed269a543b62d8e50d8dd7c81f93196b40e9
Conaxian <conax@conax.cz>
2021-10-17 14:40:30 +0200

    Add SubjectsNav component

---
+++ src/components/SubjectsNav/index.tsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+import { Button, ButtonGroup } from "@mui/material";
+
+interface Subjects {
+  zs: string;
+  bi: string;
+  ch: string;
+  cj: string;
+  aj: string;
+  ze: string;
+  de: string;
+  ma: string;
+  eh: string;
+  fy: string;
+  sj: string;
+}
+
+type SubjectCode = keyof Subjects;
+
+const SUBJECTS: Subjects = {
+  zs: "Basic Humanities",
+  bi: "Biology",
+  ch: "Chemistry",
+  cj: "Czech",
+  aj: "English",
+  ze: "Geography",
+  de: "History",
+  ma: "Mathematics",
+  eh: "Music",
+  fy: "Physics",
+  sj: "Spanish",
+};
+
+const makeButtonUrl = (code: string) =>
+  `${window.location.protocol}//${window.location.host}/notes/${code}`;
+
+function* makeButtons() {
+  for (const code in SUBJECTS) {
+    yield (
+      <Button href={makeButtonUrl(code)}>
+        {SUBJECTS[code as SubjectCode]}
+      </Button>
+    );
+  }
+}
+
+class SubjectsNav extends Component {
+  render() {
+    return <ButtonGroup variant="contained">{makeButtons()}</ButtonGroup>;
+  }
+}
+
+export default SubjectsNav;
