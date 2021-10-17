d14d475e11d5dcdfe3e0b789f36e7657f256be03
Conaxian <conax@conax.cz>
2021-10-17 20:03:54 +0200

    Remove props from NoteMenuPage

--- src/pages/NoteMenuPage.tsx
+++ src/pages/NoteMenuPage.tsx
@@ -3,13 +3,7 @@ import { Box } from "@mui/material";
 
 import SubjectsList from "../components/SubjectsList";
 
-interface Props {
-  note: string;
-}
-
 class NotePage extends Component {
-  props!: Readonly<Props>;
-
   render() {
     return (
       <Box sx={{ mx: 4, mt: 4 }}>
