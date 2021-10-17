b1704bf4cc8fc113b52e38f947620fd8d8065bc9
Conaxian <conax@conax.cz>
2021-10-17 19:00:57 +0200

    Make NoteNav fixed to the bottom

--- src/components/NoteNav/index.tsx
+++ src/components/NoteNav/index.tsx
@@ -1,27 +1,29 @@
 import { Component } from "react";
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { Box, BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { Restore as RestoreIcon } from "@mui/icons-material";
 
 class NoteNav extends Component {
   render() {
     return (
-      <BottomNavigation>
-        <BottomNavigationAction
-          label="Previous"
-          href="/notes"
-          icon={<RestoreIcon />}
-        />
-        <BottomNavigationAction
-          label="Subjects"
-          href="/notes"
-          icon={<RestoreIcon />}
-        />
-        <BottomNavigationAction
-          label="Next"
-          href="/notes"
-          icon={<RestoreIcon />}
-        />
-      </BottomNavigation>
+      <Box sx={{ position: "fixed", bottom: 0 }}>
+        <BottomNavigation>
+          <BottomNavigationAction
+            label="Previous"
+            href="/notes"
+            icon={<RestoreIcon />}
+          />
+          <BottomNavigationAction
+            label="Subjects"
+            href="/notes"
+            icon={<RestoreIcon />}
+          />
+          <BottomNavigationAction
+            label="Next"
+            href="/notes"
+            icon={<RestoreIcon />}
+          />
+        </BottomNavigation>
+      </Box>
     );
   }
 }
