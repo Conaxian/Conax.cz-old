303ca9f5ad3faacbfdc3d798c166d2bcab23deb5
Conaxian <conax@conax.cz>
2021-10-25 21:47:24 +0200

    Add Log.php

---
+++ web/conw/lib/client/Log.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Client;
+
+require "Info.php";
+
+abstract class Log {
+  static function log() {
+    $date = date("d-m-Y H:i:s");
+    $ipInfo = Info::get($_SERVER["REMOTE_ADDR"]);
+
+    $message = "
+[$date]
+{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']}
+";
+
+    $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? "(No User-Agent)";
+    $referer = $_SERVER['REFERER'] ?? "(No Referer)";
+
+    if ($ipInfo["ipInfo"]) {
+      $proxy = $ipInfo["proxy"] ? " (Proxy)" : "";
+      $message .= "
+{$ipInfo['ip']}$proxy
+$userAgent
+$referer
+{$ipInfo['city']}, {$ipInfo['country']}
+{$ipInfo['isp']}
+";
+    } else {
+      $message .= "
+{$ipInfo['ip']} (Unknown)
+$userAgent
+$referer
+";
+    }
+
+    file_put_contents("data/logs/log.txt", "$message\n", FILE_APPEND);
+  }
+}
+
+?>
