964451ae3f4451b3473d72654df9296cf2110da2
Conaxian <conax@conax.cz>
2021-10-26 13:16:42 +0200

    Modify icons in resources, add robots.txt route

--- web/conw/routes/resources.php
+++ web/conw/routes/resources.php
@@ -2,23 +2,25 @@
 
 use Router\Resource;
 
-Resource::add("^app/(.*\.js)$", "app/$1", "application/javascript");
-Resource::add("^app/(.*\.ttf)$", "app/$1", "font/ttf");
-Resource::add("^app/(.*\.woff2)$", "app/$1", "font/woff2");
-Resource::add("^app/(.*\.txt)$", "app/$1", "text/plain");
+Resource::add("^app/(.*\\.js)$", "app/$1", "application/javascript");
+Resource::add("^app/(.*\\.ttf)$", "app/$1", "font/ttf");
+Resource::add("^app/(.*\\.woff2)$", "app/$1", "font/woff2");
+Resource::add("^app/(.*\\.txt)$", "app/$1", "text/plain");
 
 Resource::add("^styles/error\\.css$", "conw/errors/style.css", "text/css");
 
 Resource::add("^manifest\\.json$", "data/manifest.json", "application/json");
 
+Resource::add("^robots\\.txt$", "data/robots.txt", "text/plain");
+
 Resource::add(
-  "^favicon\\.(png|ico)$",
-  "content/images/favicon.png",
-  "image/png",
+  "^favicon\\.ico$",
+  "content/images/favicon.ico",
+  "image/x-icon",
 );
 
 Resource::add(
-  "^icon-(192|512)\\.png$",
+  "^icon-(180|192)\\.png$",
   "content/images/$0",
   "image/png",
 );
