0df4a607f7aa662813509083f97f543904cb2b62
Conaxian <conax@conax.cz>
2021-10-27 17:12:55 +0200

    Rewrite index.php

--- web/index.php
+++ web/index.php
@@ -1,27 +1,30 @@
 <?php declare(strict_types=1);
 
 try {
-  error_reporting(E_ALL);
-  header_remove("X-Powered-By");
-  require "conw/main.php";
-} catch (Throwable) {
-  $devMode = ($_ENV["MODE"] ?? null) !== "PRODUCTION";
-
   try {
+    error_reporting(E_ALL);
+
+    header_remove("X-Powered-By");
+    header_remove("Server");
+    header_remove("Pragma");
+
+    require "conw/main.php";
+  } catch (Throwable) {
+    $devMode = ($_ENV["MODE"] ?? null) !== "PRODUCTION";
     if ($devMode) throw new Exception;
 
     require "conw/errors/ErrorPage.php";
     Errors\ErrorPage::display(500);
-  } catch (Throwable) {
-    http_response_code(500);
-    if ($devMode) exit;
+  }
+} catch (Throwable) {
+  http_response_code(500);
+  if ($devMode) exit;
 
-    echo "
+  echo "
     <h1>500 Internal Server Error</h1>
     <hr>
     <p>[<strong>FATAL</strong>] Double error: fallback error handling failed</p>
     ";
-  }
 }
 
 ?>
