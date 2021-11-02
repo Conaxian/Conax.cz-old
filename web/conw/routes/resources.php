76a1a039e9d2934360258df261282bb5e73f79e0
Conaxian <conax@conax.cz>
2021-11-02 21:40:27 +0100

    Make images and fonts not send a charset header

--- web/conw/routes/resources.php
+++ web/conw/routes/resources.php
@@ -3,8 +3,8 @@
 use Router\Resource;
 
 Resource::add("^app/(.*\\.js)$", "app/$1", "application/javascript");
-Resource::add("^app/(.*\\.ttf)$", "app/$1", "font/ttf");
-Resource::add("^app/(.*\\.woff2)$", "app/$1", "font/woff2");
+Resource::add("^app/(.*\\.ttf)$", "app/$1", "font/ttf", false);
+Resource::add("^app/(.*\\.woff2)$", "app/$1", "font/woff2", false);
 Resource::add("^app/(.*\\.txt)$", "app/$1", "text/plain");
 
 Resource::add("^styles/error\\.css$", "conw/errors/style.css", "text/css");
@@ -21,14 +21,12 @@ Resource::add(
 
 Resource::add(
   "^icon-(180|192|512)\\.png$",
-  "content/images/$0",
-  "image/png",
+  "content/images/$0", "image/png", false,
 );
 
 Resource::add(
   "^content/osmium/(logo|footer|ok|error|info|warn|music|loading)\\.png$",
-  "$0",
-  "image/png",
+  "$0", "image/png", false,
 );
 
 ?>
