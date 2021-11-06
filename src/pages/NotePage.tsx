52ae64293de42de03c9ec2e0f98641e7fb006f8f
Conaxian <conax@conax.cz>
2021-11-06 23:56:13 +0100

    Improve NotePage spacing

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -16,7 +16,14 @@ class NotePage extends Component {
   render() {
     return (
       <>
-        <Box sx={{ mx: 4, flexGrow: 1, mb: 9 }}>
+        <Box
+          sx={{
+            mx: { xs: 4, md: "10vw", lg: "15vw", xl: "20vw" },
+            flexGrow: 1,
+            mt: { md: 2, lg: 3, xl: 4 },
+            mb: { xs: 9, md: 10, lg: 11, xl: 12 },
+          }}
+        >
           <Markdown text={this.props.note} />
         </Box>
         <NoteNav subject={this.props.subject} num={this.props.num} />
