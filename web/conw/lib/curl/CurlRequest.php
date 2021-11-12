910ae0d876eccd50e8bd4d39b2be0caf0975cf09
Conaxian <conax@conax.cz>
2021-11-12 19:14:49 +0100

    Rename Request to CurlRequest

--- web/conw/lib/curl/CurlRequest.php
+++ web/conw/lib/curl/CurlRequest.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 
-namespace Req;
+namespace Curl;
 
-class Request {
+class CurlRequest {
   private ?array $response = null;
 
   function __construct(public string $url, public array $headers = []) {}
