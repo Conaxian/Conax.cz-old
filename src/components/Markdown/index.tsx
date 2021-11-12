afb6dd31186174d796cf0edc000491972db69c0f
Conaxian <conax@conax.cz>
2021-11-12 17:41:21 +0100

    Move Markdown components into separate module

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,16 +1,12 @@
 import { Component } from "react";
 import ReactMarkdown from "react-markdown";
+
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeRaw from "rehype-raw";
 import rehypeKatex from "rehype-katex";
-import {
-  Table,
-  TableBody,
-  TableHead,
-  TableRow,
-  TableCell,
-} from "@mui/material";
+
+import components from "./components";
 
 import "katex/dist/katex.min.css";
 
@@ -30,24 +26,7 @@ class Markdown extends Component {
           children={text}
           remarkPlugins={[remarkGfm, remarkMath]}
           rehypePlugins={[rehypeRaw, rehypeKatex]}
-          components={{
-            table: ({ children }) => (
-              <Table
-                children={children}
-                sx={{ maxWidth: "min(100%, 480px)" }}
-              />
-            ),
-            thead: ({ children }) => <TableHead children={children} />,
-            tbody: ({ children }) => <TableBody children={children} />,
-            tr: ({ children }) => <TableRow children={children} />,
-            th: ({ children }) => <TableCell children={children} />,
-            td: ({ children }) => <TableCell children={children} />,
-            blockquote: ({ children }) => (
-              <blockquote>
-                <i children={children} />
-              </blockquote>
-            ),
-          }}
+          components={components}
         />
       </div>
     );
