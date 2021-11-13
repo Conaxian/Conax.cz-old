280e93ae7d33a17aec3a1976d9d7fb7262a3d447
Conaxian <conax@conax.cz>
2021-11-13 17:27:51 +0100

    Modify View JSON shape

--- web/conw/views/View.php
+++ web/conw/views/View.php
@@ -8,8 +8,29 @@ class View {
     public string $keywords,
     public string $description,
     public string $page,
+    public string $type,
     public array $data = [],
   ) {}
+
+  static function static(
+    string $title,
+    string $keywords,
+    string $description,
+    string $page,
+    array $data = [],
+  ) {
+    return new self($title, $keywords, $description, $page, "static", $data);
+  }
+
+  static function app(
+    string $title,
+    string $keywords,
+    string $description,
+    string $page,
+    array $data = [],
+  ) {
+    return new self($title, $keywords, $description, $page, "app", $data);
+  }
 
   private function getNoscript() {
     ob_start();
@@ -21,7 +42,16 @@ class View {
     $title = $this->title;
     $keywords = $this->keywords;
     $description = $this->description;
-    $data = json_encode([ "page" => $this->page, "data" => $this->data ]);
+
+    $data = json_encode([
+      "page" => [
+        "id" => $this->page,
+        "type" => $this->type,
+      ],
+      "sessionId" => session_id(),
+      "data" => $this->data,
+    ], JSON_FORCE_OBJECT);
+
     $noscript = $this->getNoscript();
     require "page.phtml";
   }
