102057f32dc39aa55fc67646eeb725e844a1ee46
Conaxian <conax@conax.cz>
2021-11-17 21:38:55 +0100

    Add physics index

--- src/game/engine/classes/physics/index.ts
+++ src/game/engine/classes/physics/index.ts
@@ -1,2 +1,3 @@
-export { default as Coordinates } from "./Coordinates";
+export { Coordinates, Movement } from "./interfaces";
 export { default as Vector2 } from "./Vector2";
+export { default as Camera } from "./Camera";
