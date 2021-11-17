e57fdab4c7884c046056b9d6aedcc10e95b66d35
Conaxian <conax@conax.cz>
2021-11-17 12:27:44 +0100

    Add Object index file

---
+++ src/game/engine/classes/Object/index.ts
@@ -0,0 +1,5 @@
+export type { default as BaseObject } from "./BaseObject";
+export { default as GlobalObject } from "./GlobalObject";
+export { default as SceneObject } from "./SceneObject";
+export { default as VisibleObject } from "./VisibleObject";
+export { default as PhysicsObject } from "./PhysicsObject";
