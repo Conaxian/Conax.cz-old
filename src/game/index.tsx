509d3c4b5ddbf57ac2e63de0c5a092c676fdae6b
Conaxian <conax@conax.cz>
2021-11-17 21:09:59 +0100

    Remove old game files

--- src/game/index.tsx
+++
@@ -1,7 +0,0 @@
-import Game from "./Game";
-
-export default async function run(root: HTMLDivElement, data: object) {
-  const game = new Game();
-  game.render(root);
-  game.run();
-}
--- src/game/main.ts
+++
@@ -1,3 +0,0 @@
-import type { Application } from "pixi.js";
-
-export default async function main(app: Application) {}
